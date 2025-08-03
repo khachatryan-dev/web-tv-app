3edc572c3bc78647fe728a109e362010a9ba379d
Aram Khachatryan <khachatryana036@gmail.com>
2025-08-03 12:53:39 +0400

    fix lint bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ async function getVideos(featuredId?: string): Promise<{ Featured: Video | null;
     if (featuredId) {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/set-featured`, {
             method: 'PATCH',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: featuredId}),
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({id: featuredId}),
         });
         if (!res.ok) {
             throw new Error('Failed to fetch videos');
         }
 
-        return  await res.json();
+        return await res.json();
     } else {
         const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/videos`, {
             cache: 'no-store',
@@ -23,21 +23,25 @@ async function getVideos(featuredId?: string): Promise<{ Featured: Video | null;
             throw new Error('Failed to fetch videos');
         }
 
-        return  await res.json();
+        return await res.json();
     }
 }
 
-export default async function HomePage({ searchParams }: { searchParams: { featuredId?: string }}) {
-  const {featuredId = ''} =  await searchParams;
+export default async function HomePage({
+                                           searchParams,
+                                       }: {
+    searchParams?: { featuredId?: string };
+}) {
+    const {featuredId} = searchParams || {};
     const {Featured, TendingNow} = await getVideos(featuredId);
 
-  return (
-      <Layout>
-        <FeaturedVideo video={Featured} />
-        <section className="mt-10">
-          <h2 className="text-xl font-semibold mb-4">Trending Now</h2>
-          <TrendingCarousel videos={TendingNow}  />
-        </section>
-      </Layout>
-  );
+    return (
+        <Layout>
+            <FeaturedVideo video={Featured}/>
+            <section className="mt-10">
+                <h2 className="text-xl font-semibold mb-4">Trending Now</h2>
+                <TrendingCarousel videos={TendingNow}/>
+            </section>
+        </Layout>
+    );
 }
